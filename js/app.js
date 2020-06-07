395ffe914ec6a973fe5b575276db101341764857
Stephenchow <39304873+stephencow@users.noreply.github.com>
2020-06-07 13:13:08 -0700

    adding typing effect
    adding typing effect on header pages

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ for(let i=0; i<headerTitleArray.length; i++){
 }
 
 let index = 0;
-let timer = setInterval(animaText,60)
+let timer = setInterval(animaText,60);
 
 function animaText(){
     const span = headerTitle.querySelectorAll("span")[index];
@@ -21,5 +21,48 @@ function animaText(){
         clearInterval(timer);
         index = 0;
     }
+};
+//****************   ******************/
+// Header Text Animation
+//****************   ******************/
+const blinkWorks = ["coding","do project","learning","workout"];
+const blinkText = document.querySelector(".headerP");
+const cursor = document.querySelector(".cursor");
+let arrayIndex = 0;
+let charIndex = 0;
+
+function type(){
+    if(charIndex < blinkWorks[arrayIndex].length){
+        if(!cursor.classList.contains("typing"))
+        cursor.classList.add("typing");
+        blinkText.textContent += blinkWorks[arrayIndex].charAt(charIndex);
+        charIndex ++;
+        setTimeout(type,300);
+    }else{
+        cursor.classList.remove("typing");
+        setTimeout(erase,1000);
+    }
 }
+
+function erase(){
+    if(charIndex > 0){
+        if(!cursor.classList.contains("typing"))
+        cursor.classList.add("typing");
+        blinkText.textContent = blinkWorks[arrayIndex].substring(0,charIndex-1);
+        charIndex --;
+        setTimeout(erase,200)
+    }else{
+        cursor.classList.remove("typing");
+        arrayIndex ++;
+        if(arrayIndex >= blinkWorks.length){
+            arrayIndex = 0;
+        }
+        setTimeout(type,2000);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    type();
+})
 
